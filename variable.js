6ccb96b04fc44427fd113352e9460f1667f2283c
Your Namekarthigavarshinim@nprcolleges.org <you@example.com>
2024-11-04 15:37:10 +0530

    first

---
+++ variable.js
@@ -0,0 +1,7 @@
+let name=document.getElementsByid("init 1")
+let button=document.getElementById("button")
+button.addEventListener('click',()=>{
+   name.innerHTML="hey js"
+    name.style.colour="red"
+    name.style.backgroundcolour="orange"
+})
